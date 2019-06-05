04d51b61a178982a6652ce998780e6a1d7f6b64b
Yu Qi Liu <yuqi.liu@weareadaptive.com>
2019-06-05 15:23:47 -0400

    main timer functionalities done #1

--- src/js/components/timer.js
+++ src/js/components/timer.js
@@ -1,5 +1,3 @@
-/* eslint-disable eol-last */
-/* eslint-disable linebreak-style */
 import React from 'react';
 
 export default class Timer extends React.Component {
@@ -7,17 +5,62 @@ export default class Timer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            displayTime = "00:00:00.00"
+            displayTime: "00:00:00.00",
+            initTime: new Date()
         };
     }
 
+    // update time on main timer
     updateDisplayTime(newTime) {
         this.setState({
-            displayTime = newTime
+            displayTime: this.millisecondsToString(newTime)
         })
     }
 
+    // start timer
+    startTime(){
+        this.mainTimer = setInterval(() => {
+            this.updateDisplayTime(new Date() - this.state.initTime);
+        }, 10);
+
+    }
+
+    // stop timer
+    stopTime(){
+        clearInterval(this.mainTimer);
+    }
+
+    // reset timer
+    resetTime(){
+        this.setState({
+            displayTime: "00:00:00.00",
+            initTime: new Date()
+        })
+    }
+
+    // resume timer
+    resumeTime(){
+        this.startTime();
+    }
+
+    // convert milliseconds to string
+    millisecondsToString(milli){
+        let milliseconds = parseInt((milli%1000)/10);
+        let seconds = parseInt((milli/1000)%60);
+        let minutes = parseInt((milli/(1000*60))%60);
+        let hours = parseInt((milli/(1000*60*60))%24);
+
+        return this.pad(hours) + ":" + this.pad(minutes) + ":" + this.pad(seconds) + "." + milliseconds;
+    }
+
+    // pad single digit
+    pad(num){
+        return num >= 10 ? num : '0' + num;
+    }
+
     render() {
-        return <div id = "time" > { this.displayTime } < /div>;
+        return (
+            <div id="time">{this.state.displayTime.toString()}</div>
+        );
     }
 }
