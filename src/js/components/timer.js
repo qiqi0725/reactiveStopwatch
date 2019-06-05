fbf5f20acc45515d04416ab1f7cc9fffb19ff8f4
Vincent Boivin <vincent.boivin@weareadaptive.com>
2019-06-05 16:37:31 -0400

    Changed the Behaviour of Timer & Button #1 #2
    The behaviour of Timer has been slightly modified
    to be extended by our LapTimer and Buttons need
    more refactoring.

--- src/js/components/timer.js
+++ src/js/components/timer.js
@@ -1,110 +1,93 @@
+/* eslint-disable class-methods-use-this */
 import React from 'react';
-import Button from './button';
 
 export default class Timer extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            mainTime: "00:00:00.00",
-            lapTime: "00:00:00.00",
+            time: '00:00:00.00',
             initTime: new Date(),
-            initLapTime: new Date()
         };
     }
 
     // update time on main timer
-    updateMainTime(newTime) {
+    updatetime(newTime) {
         this.setState({
-            mainTime: this.millisecondsToString(newTime)
-        })
+            time: this.millisecondsToString(newTime),
+        });
     }
 
     // start timer
-    startTime(){
-
+    startTime() {
         // hide start, show stop
         Button.setState({
             isStartButtonVisible: false,
-            isStopButtonVisible: true
-        })
+            isStopButtonVisible: true,
+        });
 
-        this.mainTimer = setInterval(() => {
-            this.updateMainTime(new Date() - this.state.initTime);
+        this.timer = setInterval(() => {
+            this.updatetime(new Date() - this.state.initTime);
         }, 10);
-
-    }
-
-    // start lap timers
-    // indicate colors for >2 laps
-    startLapTime(){
-
-        this.lapTimer = setInterval(() => {
-            
-        })
     }
 
     // stop timer
-    stopTime(){
-
+    stopTime() {
         // hide stop, show reset resume
         Button.setState({
             isStopButtonVisible: false,
             isResetButtonVisible: true,
-            isResumeButtonVisible: true
-        })
+            isResumeButtonVisible: true,
+        });
 
-        clearInterval(this.mainTimer);
+        clearInterval(this.timer);
     }
 
     // reset timer
-    resetTime(){
-
+    resetTime() {
         // hide reset resume, show lap start
         Button.setState({
             isResetButtonVisible: false,
             isResumeButtonVisible: false,
             isLapButtonVisible: true,
-            isStartButtonVisible: true
-        })
+            isStartButtonVisible: true,
+        });
         this.setState({
-            mainTime: "00:00:00.00",
-            initTime: new Date()
-        })
+            time: '00:00:00.00',
+            initTime: new Date(),
+        });
     }
 
     // resume timer
-    resumeTime(){
-
+    resumeTime() {
         // hide resume reset, show start lap
         Button.setState({
             isResumeButtonVisible: false,
             isResetButtonVisible: false,
             isStartButtonVisible: true,
-            isLapButtonVisible: true
-        })
+            isLapButtonVisible: true,
+        });
 
         this.startTime();
     }
 
     // convert milliseconds to string
-    millisecondsToString(milli){
-        let milliseconds = parseInt((milli%1000)/10);
-        let seconds = parseInt((milli/1000)%60);
-        let minutes = parseInt((milli/(1000*60))%60);
-        let hours = parseInt((milli/(1000*60*60))%24);
+    millisecondsToString(milli) {
+        const milliseconds = parseInt((milli % 1000) / 10, 10);
+        const seconds = parseInt((milli / 1000) % 60, 10);
+        const minutes = parseInt((milli / (1000 * 60)) % 60, 10);
+        const hours = parseInt((milli / (1000 * 60 * 60)) % 24, 10);
 
-        return this.pad(hours) + ":" + this.pad(minutes) + ":" + this.pad(seconds) + "." + milliseconds;
+        return `${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)}.${milliseconds}`;
     }
 
     // pad single digit
-    pad(num){
-        return num >= 10 ? num : '0' + num;
+    pad(num) {
+        return num >= 10 ? num : `0${num}`;
     }
 
     render() {
         return (
-            <div id="time">{this.state.mainTime.toString()}</div>
+            <div id="time">{this.state.time.toString()}</div>
         );
     }
 }
